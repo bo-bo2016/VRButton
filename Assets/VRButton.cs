acf742f8d19f6c218deef25ba4be7a85059eaa2f
严玉波 <yyb0014@chijiangvr.com>
2017-09-05 18:20:54 +0800

    feat: add project

---
+++ Assets/VRButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class VRButton : MonoBehaviour
+{
+    public Color hightlightColor = new Color(225f / 255f, 200f / 255f, 150f / 255f, 1f);
+    public Color normalColor = new Color(225f / 255f, 200f / 255f, 150f / 255f, 1f);
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
